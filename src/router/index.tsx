19878384f7ffd3c7a430d2126b8e7f06ca82b59f
fxzer <fxzer8888@163.com>
2024-06-15 23:52:18 +0800

    feat: add react-router

---
+++ src/router/index.tsx
@@ -0,0 +1,44 @@
+import { lazy } from 'react';
+import { Navigate, RouteObject, RouterProvider, createBrowserRouter} from 'react-router-dom';
+
+const Login = lazy(() => import('@/pages/login'));
+const Page404 = lazy(() => import('@/pages/errors/404'));
+
+const STATIC_ROUTES: RouteObject[] = [
+  {
+    path: '/login',
+    element:<Login />,
+  },
+  {
+    path: '/404',
+    element: <Page404 />,
+  },
+  {
+    path: '*',
+    element: <Navigate to="/404" replace />,
+  },
+];
+
+const  routes:RouteObject[]  = [
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    ),
+    children: [
+    ],
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+  ...STATIC_ROUTES,
+]
+
+const router = createBrowserRouter(routes);
+
+export default function Router(){
+  return <RouterProvider router={router} />;
+}
